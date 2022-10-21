579713b19e4fe8d648d92097ade2cc2f23d0818a
VinhTieng <vinh.tieng@laichi.vn>
2022-10-21 16:23:45 +0700

    [update] some css + create new component

--- pages/q&a.js
+++ pages/q&a.js
@@ -1,10 +1,17 @@
 import DefaultLayout from '../layouts/DefaultLayout'
 import React from 'react'
+import classNames from "classnames/bind"
+
+import styles from "~/styles/pages/Q&A.module.scss"
+import QuestionLayout from '~/components/Question/QuestionLayout'
+
+const cx = classNames.bind(styles)
 
 function QAA() {
   return (
-    <div>
-      <h1>Q&A</h1>
+    <div className={cx("wrapper")}>
+      <h1 className={cx("heading")}>Câu hỏi mới nhất</h1>
+      <QuestionLayout />
     </div>
   )
 }
