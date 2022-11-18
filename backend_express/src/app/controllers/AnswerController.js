2f422ca3bb017cf2f1e6e905df1d4c2dc28a387f
haidang2001 <nchdang16012001@gmail.com>
2022-11-19 01:07:26 +0700

    [add] delete edit answer api

--- backend_express/src/app/controllers/AnswerController.js
+++ backend_express/src/app/controllers/AnswerController.js
@@ -7,10 +7,10 @@ class AnswerController {
         try {
             await AnswerModel.find({ question_id: question_id }).exec((err, result) => {
                 if (err) return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
-                if (result) {
+                if (result.length > 0) {
                     return res.status(200).json({ status: true, message: "Các câu trả lời của câu hỏi", answer: result });
                 }
-                return res.status(400).json({ status: false, message: "Không có câu hỏi này" });
+                return res.status(400).json({ status: false, message: "Câu hỏi này chưa có câu trả lời nào" });
             });
         } catch (err) {
             return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
@@ -34,6 +34,44 @@ class AnswerController {
                 });
                 await newAnswer.save();
                 return res.status(200).json({ status: true, message: "Thêm thành công câu trả lời", answer: newAnswer });
+            });
+        } catch (err) {
+            return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
+        }
+    }
+    async delete(req, res) {
+        const id = req.params.id;
+
+        try {
+            AnswerModel.findByIdAndDelete(id, (err, result) => {
+                if (err) return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
+                if (result) {
+                    return res.status(200).json({ status: true, message: `Xóa thành công câu trả lời ${id}` });
+                }
+                return res.status(400).json({ status: false, message: "Không có câu trả lời này" });
+            });
+        } catch (err) {
+            return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
+        }
+    }
+    async edit(req, res) {
+        const id = req.params.id;
+        try {
+            const oldAnswer = await AnswerModel.findById(id).exec();
+            const form = new multiparty.Form();
+            form.parse(req, async (err, fields, files) => {
+                if (err) return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
+                let editAns = fields.content ? fields.content[0] : oldAnswer.content;
+                if (editAns == "" || editAns == undefined) {
+                    return res.status(400).json({ status: false, message: "Câu trả lời không được để trống" });
+                }
+                AnswerModel.findOneAndUpdate(id, { content: editAns }, (err, result) => {
+                    if (err) return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
+                    if (result) {
+                        return res.status(200).json({ status: true, message: `Cập nhật câu trả lời ${id} thành công` });
+                    }
+                    return res.status(500).json({ status: false, message: "Không có câu trả lời này" });
+                });
             });
         } catch (err) {
             return res.status(500).json({ status: false, message: "Có lỗi xảy ra" });
--- backend_express/src/route/Answer.js
+++ backend_express/src/route/Answer.js
@@ -6,5 +6,6 @@ const checkLogin = require("../app/middleware/checkLogin");
 
 router.get("/:question_id", AnswerController.get);
 router.post("/:question_id", checkLogin, AnswerController.post);
-
+router.delete("/:id", AnswerController.delete);
+router.put("/:id", AnswerController.edit);
 module.exports = router;
