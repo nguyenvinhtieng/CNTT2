6ddee0aa9af6c39181e5ca68b06200fd145e90c9
Vinh.Tieng <vinh.tieng@laichi.vn>
2023-02-24 13:31:00 +0700

    [update] fix bug

--- frontend/layouts/DefaultLayout/DefaultLayout.jsx
+++ frontend/layouts/DefaultLayout/DefaultLayout.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Overlay from "~/components/Overlay/Overlay";
 import Header from "~/layouts/components/Header/Header";
 import Sidebar from "~/layouts/components/SideBar/Sidebar";
+import { Montserrat } from '@next/font/google'
+const montserrat = Montserrat({subsets: ['vietnamese']})
+
 export default function DefaultLayout({ children }) {
   const [isShowSidebar, setIsShowSidebar] = React.useState(false);
   const toggleSidebar = () => setIsShowSidebar(!isShowSidebar);
 
   return (
-    <>
+    <div style={{fontFamily: montserrat.style.fontFamily}}>
       <Header toggleSidebar={toggleSidebar} />
       <main className="defaultLayout">
         <Sidebar isOpen={isShowSidebar} toggle={toggleSidebar}></Sidebar>
@@ -16,6 +19,6 @@ export default function DefaultLayout({ children }) {
           <div className="container large">{children}</div>
         </div>
       </main>
-    </>
+    </div>
   );
 }
